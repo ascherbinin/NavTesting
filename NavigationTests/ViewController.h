230c78e0a2dbd3eb447d0e41e94cf924d602cba2
ascherbinin <ascherbinin@live.ru>
2015-05-29 11:01:29 +0700

    Initial Commit

---
+++ NavigationTests/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NavigationTests
+//
+//  Created by Андрей Щербинин on 29.05.15.
+//  Copyright (c) 2015 Андрей Щербинин. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NavigationTests/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NavigationTests
+//
+//  Created by Андрей Щербинин on 29.05.15.
+//  Copyright (c) 2015 Андрей Щербинин. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
